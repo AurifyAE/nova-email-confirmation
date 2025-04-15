ecd9426f7efd004044ac6b5652458a7d36470f7c
ABDUL MAJID K <majidmaji3410@gmail.com>
2025-04-15 18:04:38 +0530

    Update react-router-dom to version 7.5.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ErrorBoundary from "../src/ErrorBoundary.js";
 import ServerError from "../src/500.jsx";
 createRoot(document.getElementById("root")).render(
-  <ErrorBoundary fallback={<ServerError/>}>
-    <App />
-  </ErrorBoundary>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ErrorBoundary fallback={<ServerError />}>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
+  </React.StrictMode>
 );
